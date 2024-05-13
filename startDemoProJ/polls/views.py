12c4b409b12a71c00408b1a61901a3edbe9d9a34
chin <chinnawat.w@opsta.co.th>
2024-05-13 13:53:10 +0700

    unitTest

--- startDemoProJ/polls/views.py
+++ startDemoProJ/polls/views.py
@@ -6,19 +6,22 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.db.models import F
 from django.views import generic
-
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
+    # lte คือ operator ที่หมายถึง "น้อยกว่าหรือเท่ากับ".
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
